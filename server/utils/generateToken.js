bd16b4762911e4c9f6a17826fa96671f58d1d119
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-10-14 22:27:10 +0200

    fixing error with token

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken';
-import { ENV_VARS } from '../config/envVars.js';
+// import jwt from 'jsonwebtoken';
+// import { ENV_VARS } from '../config/envVars.js';
 
-export const generateTokenAndSendCookie = (res, id) => { 
-    const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: '15d' });
+// export const generateTokenAndSendCookie = (res, id) => { 
+//     const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: '15d' });
     
-    res.cookie("jwt-netflix", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000, // 15days 
-        httpOnly: true, //prevent XSS attack cross site scripting attack, make it not  be accessed by javascript
-        sameSite: "strict",
-        secure: ENV_VARS.NODE_ENV !== "development",
+//     res.cookie("jwt-netflix", token, {
+//         maxAge: 15 * 24 * 60 * 60 * 1000, // 15days 
+//         httpOnly: true, //prevent XSS attack cross site scripting attack, make it not  be accessed by javascript
+//         sameSite: "strict",
+//         secure: ENV_VARS.NODE_ENV !== "development",
         
-    })
-    return token
-}
+//     })
+//     return token
+// }
